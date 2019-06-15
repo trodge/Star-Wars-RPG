587def8b74f55db04a903b7b2515796f8de04359
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-06-15 11:51:32 -0700

    added jquery

--- scripts.js
+++ scripts.js
@@ -7,27 +7,24 @@ class Character {
     }
 
     display() {
-        var imageName = this.name.replace(/ /g, '-') + '.jpg';
-        this.box = document.createElement('div');
-        this.box.setAttribute('class', 'character-box');
-        this.box.style.border = '3px ' + this.color + ' solid';
-        var nameTag = document.createElement('p');
-        nameTag.setAttribute('class', 'character-tag');
-        nameTag.innerHTML = this.name;
-        this.box.appendChild(nameTag);
-        var image = document.createElement('img');
-        image.setAttribute('src', imageName);
-        image.setAttribute('width', 180);
-        this.box.appendChild(image);
-        var hpTag = document.createElement('p');
-        hpTag.setAttribute('class', 'character-tag');
-        hpTag.innerHTML = this.hp;
-        this.box.appendChild(hpTag);
+        this.box = $('<div class="character-box" style="border: 3px + ' + this.color + ' solid"');
+        var nameTag = $('<p class="character-tag">')
+        nameTag.text(this.name);
+        this.box.append(nameTag);
+        var image = $('<img src="' + this.name.replace(/ /g, '-') + '.jpg" width=180');
+        this.box.append(image);
+        var hpTag = $('<p class="character-tag">')
+        hpTag.text(this.hp);
+        this.box.append(hpTag);
+        this.box.on('click', function () {
+        });
         document.getElementById('characters-box').appendChild(this.box);
     }
 }
 
-var characters = [new Character('Luke Skywalker', 'green', 120),
-new Character('Obi-Wan Kenobi', 'blue', 100),
+var characters = [new Character('Luke Skywalker', 'green', 100),
+new Character('Obi-Wan Kenobi', 'blue', 120),
+new Character('Mace Windu', 'purple', 120),
 new Character('Darth Sidius', 'red', 150),
-new Character('Darth Maul', 'red', 100)];
+new Character('Darth Maul', 'red', 100),
+new Character('Count Dooku', 'red', 120)];
