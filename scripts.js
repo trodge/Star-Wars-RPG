b3769c1a1fe09f13fc475397cd5c3809894c7b35
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-06-15 11:18:57 -0700

    made divs inline-block

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 class Character {
-    constructor(name) {
+    constructor(name, color) {
         this.name = name;
+        this.color = color;
         this.display();
     }
 
@@ -8,14 +9,16 @@ class Character {
         var imageName = this.name.replace(/ /g, '-') + '.jpg';
         this.box = document.createElement('div');
         this.box.setAttribute('class', 'character-box');
+        this.box.style.border = '3px ' + this.color + ' solid';
         var image = document.createElement('img');
         image.setAttribute('src', imageName);
+        image.setAttribute('width', 180);
         this.box.appendChild(image);
         document.getElementById('characters-box').appendChild(this.box);
     }
 }
 
-var characters = [new Character('Luke Skywalker'),
-new Character('Obi-Wan Kenobi'),
-new Character('Darth Sidius'),
-new Character('Darth Maul')];
+var characters = [new Character('Luke Skywalker', 'green'),
+new Character('Obi-Wan Kenobi', 'blue'),
+new Character('Darth Sidius', 'red'),
+new Character('Darth Maul', 'red')];
