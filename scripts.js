3df0013719feebe6456c6c7160f87306e0e00170
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-06-15 13:32:52 -0700

    started onclick functions

--- scripts.js
+++ scripts.js
@@ -7,18 +7,51 @@ class Character {
     }
 
     display() {
-        this.box = $('<div class="character-box" style="border: 3px ' + this.color + ' solid">');
-        var nameTag = $('<p class="character-tag">')
+        // Create a div for this character with correct color border
+        var box = $('<div class="character-box" style="border: 3px ' + this.color + ' solid">');
+        // Create a p tag for character's name
+        var nameTag = $('<p class="character-tag" id="name-tag">')
         nameTag.text(this.name);
-        this.box.append(nameTag);
+        box.append(nameTag);
+        // Load this character's image
         var image = $('<img src="' + this.name.replace(/ /g, '-') + '.jpg" width=180>');
-        this.box.append(image);
-        var hpTag = $('<p class="character-tag">')
+        box.append(image);
+        // Create a p tag for character's hp
+        var hpTag = $('<p class="character-tag" id="hp-tag">')
         hpTag.text(this.hp);
-        this.box.append(hpTag);
-        this.box.on('click', function () {
+        box.append(hpTag);
+        // Store this for use inside functions
+        var that = this;
+        box.on('click', function () {
+            $('#characters-box').remove();
+            $('#player-character-box').append(box);
+            player = that;
+            for (var i = 0; i < characters.length; ++i) {
+                if (that.color === 'red' || characters[i].color === 'red') {
+                    // This is sith or other is sith.
+                    characters[i].box.on('click', function() {
+                        $('#enemies-box').empty();
+                        var attackButton = $('<button class="attack-button">');
+                        attackButton.text('Attack');
+                        // Name of defending character
+                        var defenderName = $(this).children('#name-tag').text();
+                        // Defending character found by name
+                        defender = characters[characters.findIndex(function(c) {
+                            return c.name === defenderName;
+                        })];
+                        attackButton.on('click', function () {
+                            
+                        });
+                        $('#attack-button-box').append(attackButton);
+                        $('#defender-box').append($(this));
+                    });
+                    $('#enemies-box').append(characters[i].box);
+                }
+            }
         });
-        $('#characters-box').append(this.box);
+        $('#characters-box').append(box);
+        // Store box as member of character class
+        this.box = box;
     }
 }
 
@@ -28,3 +61,6 @@ new Character('Mace Windu', 'purple', 120),
 new Character('Darth Sidius', 'red', 150),
 new Character('Darth Maul', 'red', 100),
 new Character('Count Dooku', 'red', 120)];
+
+var player;
+var defender;
